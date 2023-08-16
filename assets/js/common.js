f7d258c6056b6f9287359bf2c2f15de871789acb
DomDev126 <dom.wolf126@gmail.com>
2023-08-16 12:06:12 +0900

    kbbi_js

---
+++ assets/js/common.js
@@ -0,0 +1,8 @@
+$(".c-usage-accordion").click(function(){
+    $(this).toggleClass("js-active");
+})
+
+jQuery('a[href^=\\#]').on('click', function(event){
+    event.preventDefault();
+    jQuery('html,body').animate({scrollTop:jQuery(this.hash).offset().top - 100}, 300);
+});
